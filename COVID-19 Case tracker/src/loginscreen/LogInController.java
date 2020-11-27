6df12ae685839eae8a5abb1d13782d9e6fe62a86
Emmanouil <manousakismanou@gmail.com>
2020-11-27 17:07:08 +0200

    Added database connection

--- COVID-19 Case tracker/src/loginscreen/LogInController.java
+++ COVID-19 Case tracker/src/loginscreen/LogInController.java
@@ -10,12 +10,19 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
+import java.util.ResourceBundle;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.io.File;
+import java.net.URL;
 
 
 
 
-public class LogInController {	
-	
+
+public class LogInController {
+
 	@FXML
 	private Label loginMessageLabel;
 	@FXML
@@ -32,10 +39,10 @@ public class LogInController {
 	private Button loginButttonn;
 	@FXML
 	private Label exitLabel;
-	
+
 	public void loginButtonOnAcction(ActionEvent event) {
 		if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
-			
+			validateLogin();
 		} else {
 			loginMessageLabel.setText("Please insert your Username and Password");
 		}
@@ -46,5 +53,30 @@ public class LogInController {
 	    Stage stage = (Stage) exitLabel.getScene().getWindow();
 	    stage.close();
 	 }
-}
 
+	 public void validateLogin() {
+			DatabaseConnectionWithLogScr connectNow = new DatabaseConnectionWithLogScr();
+			Connection connectDB = connectNow.getConnection();
+
+			String verifyLogin = "SELECT count(1) FROM useracc WHERE username =  '" + usernameTextField.getText() + "' AND password ='" + enterPasswordField.getText() + " ' ";
+
+			try {
+
+				Statement statement = connectDB.createStatement();
+				ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+				while(queryResult.next()) {
+					if (queryResult.getInt(1) == 1) {
+						loginMessageLabel.setText("Congratulations!");
+					} else {
+						loginMessageLabel.setText("Invalid login. Please try again.");
+					}
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				e.getCause();
+			}
+	 }
+
+}
