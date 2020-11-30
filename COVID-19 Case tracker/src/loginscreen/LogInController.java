19b8be07587b55b386f42c55dc1cdd903c17e2cb
Emmanouil <manousakismanou@gmail.com>
2020-11-30 13:04:17 +0200

    Gave names in logincotroller and acction on login button

--- COVID-19 Case tracker/src/loginscreen/LogInController.java
+++ COVID-19 Case tracker/src/loginscreen/LogInController.java
@@ -1,21 +1,50 @@
 package loginscreen;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
 
 public class LogInController {
-	
-	 @FXML
-	 private Label exitLabel;
-	    
 
-	 @FXML
-	 void exitButtonPressed(MouseEvent event) {
-	    Stage stage = (Stage) exitLabel.getScene().getWindow();
-	    stage.close();
+	@FXML
+ 	private Label exitLabel;
+	@FXML
+	private Label loginMessageLabel;
+	@FXML
+	private ImageView brandingImageView;
+	@FXML
+	private ImageView faceImageView;
+	@FXML
+	private ImageView lockImageView;
+	@FXML
+	private TextField usernameTextField;
+	@FXML
+	private PasswordField enterPasswordField;
+	@FXML
+	private Button loginButttonn;
+	@FXML
+	private Label exitLabel;
+
+	public void loginButtonOnAcction(ActionEvent event) {
+	 if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
+		 validateLogin();
+	 } else {
+		 loginMessageLabel.setText("Please insert your Username and Password");
 	 }
-}
+ }
 
+	@FXML
+	void exitButtonPressed(MouseEvent event) {
+		 Stage stage = (Stage) exitLabel.getScene().getWindow();
+		 stage.close();
+	}
+
+	public void validateLogin() {
+
+	}
