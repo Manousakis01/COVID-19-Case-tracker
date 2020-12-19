4bf4b01908429793f3e239a7461cb6911ef53624
LeonidhaMara <t8190101@aueb.gr>
2020-12-19 16:41:52 +0200

    Orginizing Files

---
+++ DBManagement/SQLFiles/logins_CREATE+INSERT.sql
@@ -0,0 +1,19 @@
+use CovidDB;
+
+CREATE TABLE Users (id int NOT NULL AUTO_INCREMENT,
+					 first_name varchar(255) NOT NULL,
+					 last_name varchar(255) NOT NULL,
+					 email varchar(255) NOT NULL,
+					 password varchar(255)NOT NULL,
+					 PRIMARY KEY(id));
+                     
+insert into users (id, first_name, last_name, email, password) values (1, 'Taite', 'Applebee', 'tapplebee0@is.gd', 'JdDlM8TR5t');
+insert into users (id, first_name, last_name, email, password) values (2, 'Isis', 'Wash', 'iwash1@odnoklassniki.ru', 'PZ7qs2cg1');
+insert into users (id, first_name, last_name, email, password) values (3, 'Dion', 'Jagiela', 'djagiela2@weibo.com', 'PKv4f9dnWgj');
+insert into users (id, first_name, last_name, email, password) values (4, 'Virgina', 'Ruttgers', 'vruttgers3@devhub.com', 'dOvQ5MB7');
+insert into users (id, first_name, last_name, email, password) values (5, 'Sigfrid', 'Hesey', 'shesey4@princeton.edu', '0E1Ty8I');
+insert into users (id, first_name, last_name, email, password) values (6, 'Livvy', 'Woodeson', 'lwoodeson5@delicious.com', 'pi9noWU4UCu');
+insert into users (id, first_name, last_name, email, password) values (7, 'Janene', 'Greet', 'jgreet6@gravatar.com', 'CAyJ4yzDo');
+insert into users (id, first_name, last_name, email, password) values (8, 'Lazar', 'Passy', 'lpassy7@cpanel.net', 'QxoWwkT3MA');
+insert into users (id, first_name, last_name, email, password) values (9, 'Roxane', 'Peak', 'rpeak8@bizjournals.com', 'XvvTnJgl');
+insert into users (id, first_name, last_name, email, password) values (10, 'Erroll', 'Poland', 'epoland9@aol.com', 'ffNwBWxH5');
