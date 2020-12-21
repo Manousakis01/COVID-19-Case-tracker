1c88b25ca804c16eb3ed3bf8a8f02813fea562be
LeonidhaMara <t8190101@aueb.gr>
2020-12-21 11:05:49 +0200

    Fixed names.

--- DBManagement/SQLFiles/CreateTables.sql
+++ DBManagement/SQLFiles/CreateTables.sql
@@ -9,14 +9,14 @@ DROP TABLE Tested;*/
 CREATE TABLE `Tested` (
 	partSSN char (3) not null,
 	SSN varchar(11) generated always as (CONCAT(DATE_FORMAT((dateOfBirth),'%d'),
-    DATE_FORMAT((DateOfBirth),'%m'),DATE_FORMAT((dateOfBirth),'%Y'),partSSN)) not null,
-  `firstName` varchar(255) default NULL,
-  `lastName` varchar(255) default NULL,
-  `dateOfBirth` DATE not null,
-  `dateOfTest` DATE not null,
-  `location` varchar(50) default NULL,
-  `email` varchar(255) default NULL,
-  `number` varchar(100) default NULL
+		DATE_FORMAT((DateOfBirth),'%m'),DATE_FORMAT((dateOfBirth),'%Y'),partSSN)) not null,
+	firstName varchar(255) default NULL,
+	lastName varchar(255) default NULL,
+	dateOfBirth DATE not null,
+	dateOfTest DATE not null,
+	location varchar(50) default NULL,
+	email varchar(255) default NULL,
+	number varchar(100) default NULL
 );
 
 CREATE TABLE Positive (
