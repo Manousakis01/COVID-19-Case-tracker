33e508dbe9d8d5defb15c3bb9bbb893fdfbbc9da
Manousakis01 <t8190099@aueb.gr>
2021-01-13 14:35:33 +0200

    Register final view

--- COVID-19 Case tracker/src/loginscreen/RegisterController.java
+++ COVID-19 Case tracker/src/loginscreen/RegisterController.java
@@ -1,7 +1,40 @@
 package loginscreen;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
 	
+	ObservableList<String> resultList = FXCollections.observableArrayList("Positive","Negative");
 	
-
+	@FXML
+	private ChoiceBox resultBox;
+	
+	@FXML
+	private Button RegisterButton;
+	
+	@FXML
+	private TextField FirstNameTextField;
+	
+	@FXML
+	private Label FirstNameLabel;
+	
+	
+	@FXML
+	private void initialize() {
+		resultBox.setValue("Positive or Negative");
+		resultBox.setItems(resultList);
+		
+	}
+	
+	@FXML
+	void RegisterButtonOnAction(ActionEvent event) {
+		System.out.println(resultBox.getValue());
+	}
 }
