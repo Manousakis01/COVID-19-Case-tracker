214a38908abf52f5ff0ab3aa8a9e7f346faebc21
LeonidhaMara <t8190101@aueb.gr>
2021-01-18 19:24:34 +0200

    Test 1.0

--- DBManagement/SQLFiles/CreateTables.sql
+++ DBManagement/SQLFiles/CreateTables.sql
@@ -20,8 +20,8 @@ CREATE TABLE preTested (
 );
 
 CREATE TABLE IF NOT EXISTS Tested AS SELECT SSN, firstName, lastName, dateOfBirth, dateOfTest, location, email, number
- FROM preTested;
-
+ FROM preTested
+;
 
 CREATE TABLE Positive (
 	SSN varchar(11) NOT NULL,
@@ -29,28 +29,19 @@ CREATE TABLE Positive (
 );
 CREATE TABLE Meth (
 	SSN varchar(11) NOT NULL,
-<<<<<<< HEAD
 	dateOfMeth date not null,
-=======
     dateOfInsertion DATE,
->>>>>>> 79c1a225ad7dd0e51d1ed776051d80422993d5cf
     PRIMARY KEY (SSN)
 );
 CREATE TABLE Healed (
 	SSN varchar(11) NOT NULL,
-
-     dateOfHealed date not null,
-=======
+	dateOfHealed date not null,
     dateOfHeal DATE,
->>>>>>> 79c1a225ad7dd0e51d1ed776051d80422993d5cf
-    PRIMARY KEY (SSN)
+	PRIMARY KEY (SSN)
 );
 CREATE TABLE Death (
 	SSN varchar(11) NOT NULL,
-<<<<<<< HEAD
     dateOfDeath date not NULL,
-=======
     dateOfDeath DATE,
->>>>>>> 79c1a225ad7dd0e51d1ed776051d80422993d5cf
     PRIMARY KEY (SSN)
 );
