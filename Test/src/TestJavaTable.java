8f6e1d4bbb16b3f392243b9b0fbefdf1ba882f23
LeonidhaMara <t8190101@aueb.gr>
2021-01-20 20:10:44 +0200

    Test 1

--- Test/src/TestJavaTable.java
+++ Test/src/TestJavaTable.java
@@ -1,30 +1,42 @@
 import java.sql.SQLException;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import junit.framework.Assert;
 
 class TestJavaTable {
+	private String [] queries = {"SELECT * FROM Tested",//8 Column
+			"SELECT * FROM Positive", //1 Column
+			"SELECT * FROM Meth", //2
+			"SELECT * FROM Death",//2
+			"SELECT * FROM Healed",//2
+			"SELECT * FROM Users"};//2
 	@Test
 	public void create() {
 		JavaTable jt;
 		try {
 			jt = new JavaTable("Select * from Tested where SSN='12042018101'");
 			jt.printResult();
-			Assert.assertEquals("Failure-wrong name", jt.getValueAt(0, 2).toString(), "Lekatsas");
+			Assert.assertEquals("Failure-wrong name", jt.getValueAt(0, 2).toString(), "Haycroft");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	@Test
-	public void size() {
+	public void ColumnNumber() {
 		JavaTable jt;
 		try {
-			jt = new JavaTable();
+			jt = new JavaTable(queries[0]);
 			Assert.assertEquals("Failure - wrong number", jt.getColumnCount(), 8);
-		} catch (SQLException e) {
-			e.printStackTrace();
+			jt = new JavaTable(queries[1]);
+			Assert.assertEquals("Failure - wrong number", jt.getColumnCount(), 1);
+		for(int i=2; i<queries.length; i++) {
+
+				jt = new JavaTable(queries[i]);
+				Assert.assertEquals("Failure - wrong number", jt.getColumnCount(), 2);
+			}
+		}catch (SQLException e) {
+				e.printStackTrace();
 		}
 	}
-
 }
